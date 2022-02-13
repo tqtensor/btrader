e4b313c8422ba0095ce9fc1644d647d0f2662558
Tang Quoc Thai <victortang212@gmail.com>
2022-02-13 09:13:00 +0700

    Apply my format settings

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use btrader::trader::bTrader;
 use std::env;
 
 fn main() {
-  let args: Vec<String> = env::args().collect();
-  let trader: bTrader = bTrader::new(&args[1]);
-  trader.run();
+    let args: Vec<String> = env::args().collect();
+    let trader: bTrader = bTrader::new(&args[1]);
+    trader.run();
 }
